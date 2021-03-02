33672ded66153befce698ff78a6d3c5c149f7688
gqqnbig <gqqnb2005@gmail.com>
2021-03-02 15:32:48 +0800

    Create script for adding users to all nodes
    Allow to use sudo adduser/addgroup without password.
    
    Host server is going to run these command remotely. No password makes it scriptable.

---
+++ cluster-add-user/cluster-add-user.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+username=$1
+sudo adduser --conf adduser.conf --disabled-password $username
+
+# create shared folder
+sudo mkdir -p /home/shared/$username
+sudo chown $username:$username /home/shared/$username
+sudo ln -s /home/shared/$username /home/$username/shared
+
+uid=$(id -u $username)
+gid=$(id -g $username)
+
+gecos=$(getent passwd $username | cut -d ':' -f 5)
+
+cat <<HERE > ~/shared/remote-add-user
+#!/bin/bash -e
+sudo addgroup --gid $gid $username
+# It's not documented clearly, but --gid requires the existence of GID.
+sudo adduser --gecos "$gecos" --disabled-login --uid $uid --gid $gid $username
+
+sudo ln -s /home/shared/$username /home/$username/shared
+HERE
+
+chmod +x ~/shared/remote-add-user
+
+if ! ssh eureka ~/shared/remote-add-user; then
+	echo "Failed to execute remote command,"
+	echo "Please run ~/shared/remote-add-user on eureka"
+fi
+
+read -p "In the next screen, you will paste public key. Press enter to continue."
+sudo mkdir -p /home/$username/.ssh
+sudoedit /home/$username/.ssh/authorized_keys
+sudo chmod 700 /home/$username/.ssh
+sudo chmod 600 /home/$username/.ssh/authorized_keys
+sudo chown -R $username:$username /home/$username/.ssh
---
+++ cluster-add-user/install.sh
@@ -0,0 +1,3 @@
+# For security reason, do not link the file.
+sudo cp ~/mpi-servers/cluster-add-user/etc/sudoers.d/cluster-add-user /etc/sudoers.d/cluster-add-user
+sudo chown root:root /etc/sudoers.d/cluster-add-user
