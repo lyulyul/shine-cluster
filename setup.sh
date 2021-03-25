f2463840be4e7e1f7a179328d2cf2c36668c4c74
QQ喵 <gqqnb2005@gmail.com>
2021-03-25 14:48:42 +0800

    Add common setup for compute and login

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -e
+
+sudo timedatectl set-timezone Asia/Macau
+sudo systemctl isolate multi-user.target #立即运行multi-user.target，禁用其他target
+sudo systemctl set-default multi-user.target #开机启动至multi-user.target
+
+sudo update-grub
+sudo apt update
+sudo apt upgrade
+
+sudo addgroup wheel
+sudo chown :wheel /usr/bin/su
+sudo chmod 4754 /usr/bin/su
+sudo usermod -aG wheel $(whoami)
+
+
+# 系统优化
+sudo systemctl disable motd-news
+sudo chmod -x /etc/update-motd.d/50-motd-news
+
