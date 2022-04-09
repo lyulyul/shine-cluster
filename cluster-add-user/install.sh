8090bb25a1f854d486dd73bfba200620c1e9d45a
gqqnbig <gqqnb2005@gmail.com>
2022-04-09 16:06:41 +0800

    rename repository to shine-cluster

--- cluster-add-user/install.sh
+++ cluster-add-user/install.sh
@@ -1,2 +1,2 @@
 # For security reason, do not link the file.
-sudo cp --no-preserve=ownership ~/mpi-servers/cluster-add-user/etc/sudoers.d/cluster-add-user /etc/sudoers.d/cluster-add-user
+sudo cp --no-preserve=ownership ~/shine-cluster/cluster-add-user/etc/sudoers.d/cluster-add-user /etc/sudoers.d/cluster-add-user
