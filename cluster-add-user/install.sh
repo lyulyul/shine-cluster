189149d46c2f470b3c130dfe0f8a435ec990e4a5
QQ喵 <gqqnb2005@gmail.com>
2022-05-17 15:26:46 +0800

    no preserve ownership so that we don't need a second `chown`

--- cluster-add-user/install.sh
+++ cluster-add-user/install.sh
@@ -1,3 +1,2 @@
 # For security reason, do not link the file.
-sudo cp ~/mpi-servers/cluster-add-user/etc/sudoers.d/cluster-add-user /etc/sudoers.d/cluster-add-user
-sudo chown root:root /etc/sudoers.d/cluster-add-user
+sudo cp --no-preserve=ownership ~/mpi-servers/cluster-add-user/etc/sudoers.d/cluster-add-user /etc/sudoers.d/cluster-add-user
